42135e09a635a8852d855a2eb34c4f9c03258fbc
yosef264 <69306966+yosef264@users.noreply.github.com>
2025-07-19 21:09:23 +0300

    ccolor implementation

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <><div className="bg-primary">Hello</div></>
+    <>
+    {/* <div className="bg-primary">Hello</div> */}
+    <div className="from-primary to-secondary bg-gradient-to-l min-h-screen"> gradient</div></>
   );
 }
