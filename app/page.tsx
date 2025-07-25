56e752995241c9b179a62c94be463c99f622918f
yosef264 <69306966+yosef264@users.noreply.github.com>
2025-07-25 15:39:57 +0300

    some header work

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <Button className="p-6 text-xl m-2">Suck Me</Button>
     </>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import { House, Search } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+export default function Header() {
+  return (
+    <>
+      <header className="flex p-4 justify-between bg-light">
+        {/* Icon and Name part */}
+        <div className="flex">
+          <House className="bg-primary text-lighter rounded-lg h-12 w-12 p-1 " />
+          <div className="pl-2 text-2xl">
+            <h2 className="">HomeBites</h2>
+            <p className="text-[10px]">Local Food Network</p>
+          </div>
+        </div>
+        {/* Food and about us part */}
+        <div className="content-center">
+          <Link href="/food" className="pr-4">
+            Find Food
+          </Link>
+          <Link href="/host" className="pr-4">
+            Become a Host
+          </Link>
+          <Link href="/about">About Us</Link>
+        </div>
+        {/* Search and log in part */}
+        <div className="content-center flex">
+          <Button>
+            Search
+            <Search h-4 w-4 />
+          </Button>
+        </div>
+      </header>
+    </>
+  );
+}
